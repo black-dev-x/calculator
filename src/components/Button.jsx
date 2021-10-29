b053ba61109d78439927df33deb2e80c5c78c9ec
black.dev.x <thiago.dev.x.@gmail.com>
2021-10-29 15:21:31 -0300

    updating the calculator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
 import './Button.css'
 import React from 'react'
 
-const Button = props => <button>{props.label}</button>
+const Button = props => (
+  <button
+    onClick={_ => props.click(props.label)}
+    className={`
+        Button 
+        ${props.operation ? 'operation' : ''} 
+        ${props.double ? 'double' : ''} 
+        ${props.triple ? 'triple' : ''}
+    `}
+  >
+    {props.label}
+  </button>
+)
 
 export default Button
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,32 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../components/Button'
 import Display from '../components/Display'
 import './Calculator.css'
 
-const Calculator = () => (
-  <>
-    <h1>Calculadora</h1>
-    <div className='Calculator'>
-      <Display value={100} />
-      <Button label='AC' />
-      <Button label='/' />
-      <Button label='7' />
-      <Button label='8' />
-      <Button label='9' />
-      <Button label='*' />
-      <Button label='4' />
-      <Button label='5' />
-      <Button label='6' />
-      <Button label='-' />
-      <Button label='1' />
-      <Button label='2' />
-      <Button label='3' />
-      <Button label='+' />
-      <Button label='0' />
-      <Button label='.' />
-      <Button label='=' />
-    </div>
-  </>
-)
+const initialState = {
+  display: '0',
+  clearDisplay: true,
+  operation: null,
+  values: [0, 0],
+  index: 0
+}
+const Calculator = () => {
+  const [state, setState] = useState(initialState)
+
+  const clearMemory = () => setState(initialState)
+
+  const setOperation = operation => {
+    const index = state.index
+    const number = +state.display
+    const values = [...state.values]
+    values[index] = number
+    if (index === 0) {
+      setState({ ...state, values, operation, clearDisplay: true, index: 1 })
+    } else {
+      const equals = operation === '='
+      const currentOperation = state.operation
+      values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+      values[1] = 0
+      setState({ ...state, values, display: values[0], clearDisplay: !equals, operation: equals ? null : operation, index: equals ? 0 : 1 })
+    }
+  }
+
+  const digitPressed = digit => {
+    if (digit === '.' && state.display.includes('.')) return
+    const currentValue = state.clearDisplay ? '' : state.display
+    const nextDisplay = currentValue + digit
+    setState({ ...state, display: nextDisplay, clearDisplay: false })
+  }
+
+  return (
+    <>
+      <h1>Calculadora</h1>
+      <div className='Calculator'>
+        <Display value={state.display} />
+        <Button label='AC' triple click={clearMemory} />
+        <Button label='/' operation click={setOperation} />
+        <Button label='7' click={digitPressed} />
+        <Button label='8' click={digitPressed} />
+        <Button label='9' click={digitPressed} />
+        <Button label='*' operation click={setOperation} />
+        <Button label='4' click={digitPressed} />
+        <Button label='5' click={digitPressed} />
+        <Button label='6' click={digitPressed} />
+        <Button label='-' operation click={setOperation} />
+        <Button label='1' click={digitPressed} />
+        <Button label='2' click={digitPressed} />
+        <Button label='3' click={digitPressed} />
+        <Button label='+' operation click={setOperation} />
+        <Button label='0' double click={digitPressed} />
+        <Button label='.' click={digitPressed} />
+        <Button label='=' operation click={setOperation} />
+      </div>
+    </>
+  )
+}
 
 export default Calculator
